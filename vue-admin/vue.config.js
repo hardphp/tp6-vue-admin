0813577c2626952116c44d5e3bdfd0ecd989ea26
hardphp <hardphp@163.com>
2020-04-15 13:08:58 +0800

    Update vue.config.js

--- vue-admin/vue.config.js
+++ vue-admin/vue.config.js
@@ -27,19 +27,6 @@ module.exports = {
   indexPath: 'index.html',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
-  pages: {
-    index: {
-      // page 的入口
-      entry: './src/main.js',
-      // 模板来源
-      template: './public/index.html',
-      // 在 dist/index.html 的输出
-      filename: 'index.html'
-      // 在这个页面中包含的块，默认情况下会包含
-      // 提取出来的通用 chunk 和 vendor chunk。
-      // chunks: ['chunk-vendors', 'chunk-common', 'index']
-    }
-  },
   devServer: {
     port: port,
     open: false,
